3c6c4434e5df72b1c7e0fb4e9678a8a9811876e2
djinnsend <nanaboakye1738@gmail.com>
2021-03-10 18:03:57 +0000

    Credentials() not complete

--- Voter.java
+++ Voter.java
@@ -1,4 +1,4 @@
-class Voter{
+class Voter extends Person{
     String voterID;
     String pollingStation;
     String homeTown;
@@ -7,8 +7,28 @@ class Voter{
     String votedList;
     String viewChoice;
 
-    public Voter(String voterID, String pollingStation, String homeTown, String candidate, String password, String votedList, String viewChoice){
-        System.out.println("hello");
+    public Voter(String voterID, String pollingStation, String homeTown, String candidate, String password, String votedList, String viewChoice,String name, String dateofBirth, String gender, int age){
+        super(name,dateofBirth,gender,age);
+        this.voterID = voterID;
+        this.pollingStation = pollingStation;
+        this.homeTown = homeTown;
+        this.candidate = candidate;
+        this.password = password;
+        this.votedList = votedList;
+        this.viewChoice = viewChoice;
     }
+
+    // public String getDistrictName(){
+    //     return "1";
+    // }
+
+    public String getName(){
+        return super.getName();
+    }
+
+    public String credentials(){
+        return "dummy0";
+    }
+
 
 }
