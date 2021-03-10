190d40cadd8312b70f059cd553adfdc0c34c9598
djinnsend <nanaboakye1738@gmail.com>
2021-03-10 17:33:09 +0000

    Create Voter.java

---
+++ Voter.java
@@ -0,0 +1,14 @@
+class Voter{
+    String voterID;
+    String pollingStation;
+    String homeTown;
+    String candidate;
+    String password;
+    String votedList;
+    String viewChoice;
+
+    public Voter(String voterID, String pollingStation, String homeTown, String candidate, String password, String votedList, String viewChoice){
+        System.out.println("hello");
+    }
+
+}
